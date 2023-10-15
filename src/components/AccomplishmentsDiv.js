21e860e27bee8ffd8067a1a111b677a8655f31a0
tejjgv <143538529+tejjgv@users.noreply.github.com>
2023-10-15 15:23:54 +0530

    Update AccomplishmentsDiv.js

--- src/components/AccomplishmentsDiv.js
+++ src/components/AccomplishmentsDiv.js
@@ -22,7 +22,7 @@ let accomplishmentsContent = [
     emoji: "🎲",
     name: "Housie Game",
     keyPoints: "💻 Made using JavaScript, HTML, CSS.",
-    link: "hhttps://tejjgv.github.io/game-2/",
+    link: "https://tejjgv.github.io/game-2/",
     linkText: "Visit Site",
     img: HousieGameImg,
     external: true,
